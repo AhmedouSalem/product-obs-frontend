63cb86c93f52a70f1ade8ae746230ad37af5c191
AhmedouSalem <ahmedou8salem@gmail.com>
2026-05-12 23:35:11 +0200

    Allow video screen

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: "http://localhost:4200",
     setupNodeEvents(on, config) {
